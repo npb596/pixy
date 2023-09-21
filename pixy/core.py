8a128f962cc7665ea9d47761e43ea57b113361e4
Nick Bailey <npb596@gmail.com>
2023-09-21 07:12:14 -0500

    Fixed error where tajima_d function could only run when other functions were called

--- pixy/core.py
+++ pixy/core.py
@@ -554,11 +554,13 @@ def compute_summary_stats(args, popnames, popindices, temp_file, chromosome, chu
                         no_sites = np.count_nonzero(np.sum(allele_counts, 1))
                         tajima_d, pi, watterson_theta, d_stdev = calc.calc_tajima_d(gt_pop)
 
-                # create a string of the pi results to write to file
+                # create a string of the Tajima's D results to write to file
                 #klk added NA so that pi/dxy/fst have the same # of columns, npb has kept this for Tajima's D
                 pixy_result = "tajima_d" + "\t" + str(pop) + "\tNA\t" + str(chromosome) + "\t" + str(window_pos_1) + "\t" + str(window_pos_2) + "\t" + str(tajima_d) + "\t" + str(no_sites) + "\t" + str(pi) + "\t" + str(watterson_theta) + "\t" + str(d_stdev)
                 if 'pixy_output' in locals():
                     pixy_output = pixy_output + "\n" + pixy_result
+                else:
+                    pixy_output = pixy_result
 
     # OUTPUT
     # if in mc mode, put the results in the writing queue
